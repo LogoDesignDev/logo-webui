bbebe7bd75c7cbb6075eb7f46b5fd93f7eb887ec
nisuiyiyo <2363094191@qq.com>
2020-06-04 10:53:24 +0800

    gallery

--- src/main/java/com/example/mylogo/entity/Gallery.java
+++ src/main/java/com/example/mylogo/entity/Gallery.java
@@ -1,6 +1,7 @@
 package com.example.mylogo.entity;
 
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.text.SimpleDateFormat;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @Document(collection = "gallery")
 public class Gallery {
+    @Id
     private ObjectId galleryId;//图库id
     private String name;//图库名称
     private String region;//图库权限
